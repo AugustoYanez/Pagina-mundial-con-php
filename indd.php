b0b1372d507407dffbf07aec971ac2995d45ac9d
Augusst <101185871+AugustoYanez@users.noreply.github.com>
2022-11-23 11:46:01 -0300

    Add files via upload

---
+++ indd.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/1logo.ico">
+    <title> Fifa World Cup Qatar 2022 ™ </title>
+</head>
+<body>
+
+    <header class="headprincipal">
+
+
+        <div class="container">
+            <p class="logo"> FIFA WORLD CUP QATAR 2022 </p>
+           <nav class="navmenu">
+            <a href="#"> Noticias </a>  <a href="#"> Partidos </a> <a href="#"> Equipos </a>   <a href="#"> Otros </a>    
+            <a href="index.php"> Mi Cuenta </a>   
+           </nav>
+        </div>
+    </header>
+
+    
+<section id="hero">
+
+    <h1> ¡ ARRANCO EL MUNDIAL ! <br> QATAR 2022 </h1>
+    <form action="#">
+    <button> Mira el fixture aca.</button>
+</form>
+</section>
+
+
+
+
+
+            <section id="mundiales">
+
+                <div class="container">
+
+            
+            <h2 id="idmun"> ULTIMOS MUNDIALES </h2>
+
+            <div class="otromundiales">
+
+            <div class="Mmundiales">
+                <h3> Mundial Sudafrica 2010</h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem, enim vitae ex error odit fuga magni inventore? Voluptas laboriosam non odio inventore architecto consequatur consectetur dolor soluta ex qui?</p>
+            <form action="https://www.youtube.com/watch?v=PLrQJKU6e-g">
+            <button> Ver </button>
+        </form>
+
+            </div>
+
+            <div class="Mmundiales">
+                <h3>Mundial Brasil<br> 2014</h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem, enim vitae ex error odit fuga magni inventore? Voluptas laboriosam non odio inventore architecto consequatur consectetur dolor soluta ex qui?</p>
+            <form action="#">
+            <button> Ver </button>
+        </form>
+            </div>
+            <div class="Mmundiales">
+                <h3>Mundial Rusia <br>2018</h3>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam autem, enim vitae ex error odit fuga magni inventore? Voluptas laboriosam non odio inventore architecto consequatur consectetur dolor soluta ex qui?</p>     
+            <form action="#">
+            <button> Ver </button>
+        </form>
+    </div>
+            </div>
+            </div>
+
+
+
+</section>
+
+
+
+</body>
+</html>
